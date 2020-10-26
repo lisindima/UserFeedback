d01446c154f4a0293b9b32402fa40c61b2531524
lisindima <lisindima1996@gmail.com>
2020-10-26 17:17:28 +0700

    Update UserFeedback.swift

--- Sources/UserFeedback/UserFeedback.swift
+++ Sources/UserFeedback/UserFeedback.swift
@@ -9,7 +9,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedFeedback: TopicFeedback = .bug
+    @State private var typeFeedback: TypeFeedback = .bug
+    @State private var typeReproduce: TypeReproduce = .yes_every_time
+    @State private var diagnosticsInclude: Bool = false
     @State private var name: String = "Дмитрий"
     @State private var email: String = "lisindima1996@gmail.com"
     
@@ -17,15 +19,35 @@ struct ContentView: View {
         Form {
             Section(header: Text("Выберите подходящий вариант из списка").fontWeight(.bold)) {
                 Picker(
-                    selection: $selectedFeedback,
+                    selection: $typeFeedback,
                     label: Text("")
                 ) {
                     Text("Ошибка в приложении")
-                        .tag(TopicFeedback.bug)
+                        .tag(TypeFeedback.bug)
                     Text("Запрос функции")
-                        .tag(TopicFeedback.feature_request)
+                        .tag(TypeFeedback.feature_request)
                     Text("Другое")
-                        .tag(TopicFeedback.other)
+                        .tag(TypeFeedback.other)
+                }
+            }
+            if typeFeedback == .bug {
+                Section(header: Text("Вы можете воспроизвести ошибку?").fontWeight(.bold)) {
+                    Picker(
+                        selection: $typeReproduce,
+                        label: Text("")
+                    ) {
+                        Text("Да, каждый раз")
+                            .tag(TypeReproduce.yes_every_time)
+                        Text("Да, иногда")
+                            .tag(TypeReproduce.yes_sometimes)
+                        Text("Да, редко")
+                            .tag(TypeReproduce.yes_rarely)
+                        Text("Нет")
+                            .tag(TypeReproduce.no)
+                    }
+                }
+                Section(header: Text("Диагностика").fontWeight(.bold)) {
+                    Toggle("Прикрепить файл диагностики", isOn: $diagnosticsInclude)
                 }
             }
             Section(header: Text("Укажите вашу контактную информацию").fontWeight(.bold)) {
@@ -36,10 +58,17 @@ struct ContentView: View {
     }
 }
 
-enum TopicFeedback: String {
+enum TypeFeedback: String {
     case bug = "Ошибка в приложении"
     case feature_request = "Запрос функции"
     case other = "Другое"
+}
+
+enum TypeReproduce: String {
+    case yes_every_time = "Да, каждый раз"
+    case yes_sometimes = "Да, иногда"
+    case yes_rarely = "Да, редко"
+    case no = "Нет"
 }
 
 struct UserFeedbackModifier: ViewModifier {
