a9c3955ea1bf90e2544610f95d8bc463e2488a4d
lisindima <lisindima1996@gmail.com>
2020-10-26 16:39:38 +0700

    Update UserFeedback.swift

--- Sources/UserFeedback/UserFeedback.swift
+++ Sources/UserFeedback/UserFeedback.swift
@@ -1,9 +1,45 @@
+//
+//  UserFeedback.swift
+//  UserFeedback
+//
+//  Created by Дмитрий Лисин on 26.10.2020.
+//  Copyright © 2020 Дмитрий Лисин. All rights reserved.
+//
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedFeedback: TopicFeedback = .bug
+    @State private var name: String = "Дмитрий"
+    @State private var email: String = "lisindima1996@gmail.com"
+    
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            Section(header: Text("Выберите подходящий вариант из списка").fontWeight(.bold)) {
+                Picker(
+                    selection: $selectedFeedback,
+                    label: Text("")
+                ) {
+                    Text("Ошибка в приложении")
+                        .tag(TopicFeedback.bug)
+                    Text("Запрос функции")
+                        .tag(TopicFeedback.feature_request)
+                    Text("Другое")
+                        .tag(TopicFeedback.other)
+                }
+            }
+            Section(header: Text("Укажите вашу контактную информацию").fontWeight(.bold)) {
+                TextField("Имя", text: $name)
+                TextField("Почта", text: $email)
+            }
+        }
     }
+}
+
+enum TopicFeedback: String {
+    case bug = "Ошибка в приложении"
+    case feature_request = "Запрос функции"
+    case other = "Другое"
 }
 
 struct UserFeedbackModifier: ViewModifier {
@@ -15,7 +51,7 @@ struct UserFeedbackModifier: ViewModifier {
                 NavigationView {
                     ContentView()
                         .ignoresSafeArea(edges: .bottom)
-                        .navigationTitle("Сообщение об ошибке")
+                        .navigationTitle("Обратная связь")
                         .toolbar {
                             ToolbarItem(placement: .primaryAction) {
                                 Button(action: { isPresented = false }) {
