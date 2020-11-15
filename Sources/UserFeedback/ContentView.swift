3f0d9afff80a1764491cc2f1c2c30060f1df5e58
lisindima <lisindima1996@gmail.com>
2020-11-15 20:31:24 +0700

    Кнопка

--- Sources/UserFeedback/ContentView.swift
+++ Sources/UserFeedback/ContentView.swift
@@ -18,47 +18,58 @@ struct ContentView: View {
     @State private var descriptionFeedback: String = ""
     
     var body: some View {
-        Form {
-            Section(header: Text("Основное").fontWeight(.bold)) {
-                Picker(selection: $typeFeedback, label: Text("Какой тип отзыва вы хотите отправить?")) {
-                    Text("Ошибка в приложении")
-                        .tag(TypeFeedback.bug)
-                    Text("Запрос функции")
-                        .tag(TypeFeedback.feature_request)
-                    Text("Другое")
-                        .tag(TypeFeedback.other)
-                }
-                if typeFeedback == .bug {
-                    Picker(selection: $typeReproduce, label: Text("Вы можете воспроизвести ошибку?")) {
-                        Text("Да, каждый раз")
-                            .tag(TypeReproduce.yes_every_time)
-                        Text("Да, иногда")
-                            .tag(TypeReproduce.yes_sometimes)
-                        Text("Да, редко")
-                            .tag(TypeReproduce.yes_rarely)
-                        Text("Нет")
-                            .tag(TypeReproduce.no)
+        VStack {
+            Form {
+                Section(header: Text("Основное").fontWeight(.bold)) {
+                    Picker(selection: $typeFeedback, label: Text("Какой тип отзыва вы хотите отправить?")) {
+                        Text("Ошибка в приложении")
+                            .tag(TypeFeedback.bug)
+                        Text("Запрос функции")
+                            .tag(TypeFeedback.feature_request)
+                        Text("Другое")
+                            .tag(TypeFeedback.other)
+                    }
+                    if typeFeedback == .bug {
+                        Picker(selection: $typeReproduce, label: Text("Вы можете воспроизвести ошибку?")) {
+                            Text("Да, каждый раз")
+                                .tag(TypeReproduce.yes_every_time)
+                            Text("Да, иногда")
+                                .tag(TypeReproduce.yes_sometimes)
+                            Text("Да, редко")
+                                .tag(TypeReproduce.yes_rarely)
+                            Text("Нет")
+                                .tag(TypeReproduce.no)
+                        }
                     }
                 }
-            }
-            if typeFeedback == .bug {
-                Section(header: Text("Диагностика").fontWeight(.bold), footer: Text("Файлы диагностики могут включать такую ​​информацию, как модель вашего устройства, версия операционной системы.")) {
-                    Toggle("Прикрепить файл диагностики", isOn: $diagnosticsInclude)
+                if typeFeedback == .bug {
+                    Section(header: Text("Диагностика").fontWeight(.bold), footer: Text("Файлы диагностики могут включать такую ​​информацию, как модель вашего устройства, версия операционной системы.")) {
+                        Toggle("Прикрепить файл диагностики", isOn: $diagnosticsInclude)
+                    }
+                }
+                Section(header: Text("Контактная информация").fontWeight(.bold)) {
+                    TextField("Имя", text: $name)
+                    TextField("Email", text: $email)
+                }
+                Section(header: Text("Описание").fontWeight(.bold)) {
+                    TextField("Краткое описание проблемы", text: $titleFeedback)
+                    #if os(watchOS)
+                    TextField("Полное описание проблемы", text: $descriptionFeedback)
+                    #else
+                    TextEditor(text: $descriptionFeedback)
+                        .frame(height: 300)
+                    #endif
                 }
             }
-            Section(header: Text("Контактная информация").fontWeight(.bold)) {
-                TextField("Имя", text: $name)
-                TextField("Email", text: $email)
-            }
-            Section(header: Text("Описание").fontWeight(.bold)) {
-                TextField("Краткое описание проблемы", text: $titleFeedback)
-                #if os(watchOS)
-                TextField("Полное описание проблемы", text: $descriptionFeedback)
-                #else
-                TextEditor(text: $descriptionFeedback)
-                    .frame(height: 300)
-                #endif
-            }
+            SendButton(
+                title: "Отправить",
+                loading: false,
+                colorButton: .red,
+                colorText: .white,
+                action: {}
+            )
+            .padding(.horizontal)
+            .padding(.bottom, 8)
         }
     }
 }
---
+++ Sources/UserFeedback/Extensions/Button.swift
@@ -0,0 +1,54 @@
+//
+//  Button.swift
+//  
+//
+//  Created by Дмитрий Лисин on 14.11.2020.
+//
+
+import SwiftUI
+
+struct SendButton: View {
+    var title: String
+    var loading: Bool = false
+    var colorButton: Color = .red
+    var colorText: Color = .white
+    var action: () -> Void
+    
+    var body: some View {
+        #if os(iOS)
+        phone
+        #else
+        watch
+        #endif
+    }
+    
+    var watch: some View {
+        Button(action: action) {
+            if loading {
+                ProgressView()
+            } else {
+                Text(title)
+            }
+        }
+    }
+    
+    var phone: some View {
+        Button(action: action) {
+            if loading {
+                ProgressView()
+                    .padding()
+            } else {
+                HStack {
+                    Spacer()
+                    Text(title)
+                        .fontWeight(.bold)
+                        .foregroundColor(colorText)
+                    Spacer()
+                }.padding()
+            }
+        }
+        .disabled(loading)
+        .background(colorButton)
+        .cornerRadius(8)
+    }
+}
